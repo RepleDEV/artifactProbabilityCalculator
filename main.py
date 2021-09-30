496b74b94994d7fcc0fadcacb96b21e29615e31b
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-09-30 13:52:10 +0700

    Probability rescale

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Probabilities: list of numbers
+def rescaleProbabilities(probabilities, probability_to_remove):
+    probabilities.remove(probability_to_remove)
+    sum_of_probabilities = sum(probabilities)
+    scaling = 100 / sum_of_probabilities
+
+    return list(map(lambda x: x * scaling, probabilities))
+
+print(rescaleProbabilities([25, 25, 50], 50))
+
 def main(artifact_type, main_stat, substat_1, substat_2, substat_3, substat_4, full_substats=False):
     current_probability = 1
     # Step 1, set probability
